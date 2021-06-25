3d3e27453903c48159494dc30ea3c05099955056
Fatensamman <arch.fatensamman@gmail.com>
2021-06-25 17:43:07 +0300

    creat-header-footer

---
+++ src/component/Header.js
@@ -0,0 +1,44 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, AppBar, Toolbar ,Button} from '@material-ui/core';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+
+
+const useStyles = makeStyles(() => ({
+  button:
+  {
+    color: 'white',
+    fontSize: "1em",
+  },
+
+  title: {
+    flexGrow: 1,
+    color: 'white',
+    fontSize: "2em",
+  },
+
+  navBar:
+  {
+    background: '#586a89',
+  },
+})
+);
+
+function Header(props) {
+  const classes = useStyles();
+  return (
+    <AppBar position="static" className={classes.navBar}>
+      <Toolbar>
+        <NavLink to="/" variant="h6" className={classes.title} style={{ textDecoration: 'none' }}> Mrs.Cart </NavLink>
+        <Button  className={classes.button} color="inherit" >
+        <NavLink activeClassName="selected" to="/cart" className='MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-colorInherit' >
+        <ShoppingCartIcon/>
+          </NavLink>
+        </Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header;
